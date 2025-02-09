9b58e6b57f5dbdd034cb698e827cfa9a0dd8f0f1
orewaee <orewaee@gmail.com>
2025-02-09 12:18:17 +0400

    chore: tidy up the docker compose config

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker compose -f ./deploy/compose.yaml -p vortex up -d
+ENV_FILE_PATH=config/.env
+FILE_PATH=deploy/compose.yaml
+PROJECT_NAME=vortex
+
+docker compose \
+  --env-file config/.env \
+  --file $FILE_PATH \
+  --project-name $PROJECT_NAME \
+  up --detach
