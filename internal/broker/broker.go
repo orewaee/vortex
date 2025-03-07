6f4b572aaddc2dab74b5fb90de22a3298eee1cd0
orewaee <orewaee@gmail.com>
2025-03-07 23:23:35 +0400

    fix: minor broker changes

--- internal/broker/broker.go
+++ internal/broker/broker.go
@@ -1,7 +1,10 @@
 package broker
 
+import "sync"
+
 type Broker[T any] struct {
 	subscribers map[chan T]any
+	mu          sync.RWMutex
 }
 
 func New[T any]() *Broker[T] {
@@ -11,17 +14,42 @@ func New[T any]() *Broker[T] {
 }
 
 func (broker *Broker[T]) Subscribe() chan T {
-	message := make(chan T)
+	broker.mu.Lock()
+	defer broker.mu.Unlock()
+
+	message := make(chan T, 128)
 	broker.subscribers[message] = nil
 	return message
 }
 
 func (broker *Broker[T]) Unsubscribe(message chan T) {
-	delete(broker.subscribers, message)
+	broker.mu.Lock()
+	defer broker.mu.Unlock()
+
+	if _, ok := broker.subscribers[message]; ok {
+		delete(broker.subscribers, message)
+		close(message)
+	}
 }
 
 func (broker *Broker[T]) Publish(message T) {
+	broker.mu.RLock()
+	defer broker.mu.RUnlock()
+
 	for subscriber := range broker.subscribers {
-		subscriber <- message
+		select {
+		case subscriber <- message:
+		default:
+		}
+	}
+}
+
+func (broker *Broker[T]) Shutdown() {
+	broker.mu.Lock()
+	defer broker.mu.Unlock()
+
+	for subscriber := range broker.subscribers {
+		delete(broker.subscribers, subscriber)
+		close(subscriber)
 	}
 }
